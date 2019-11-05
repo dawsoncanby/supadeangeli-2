58d8ced9311fe4ed0fdff3f10a612b77f31390b2
dawsoncanby <dawsoncanby@gmail.com>
2019-11-04 23:33:07 -0700

    added new releases

--- js/startup.js
+++ js/startup.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+  // add images
+  $('#release-img-container').prepend('<img src="img/talk_that_way.jpg" alt="new release" style="display: block;margin: auto;"></img>');
+  $('#release-img-container').click(function() {
+    
+  });
+  // fade in content
   $('#page-header').fadeIn(1000);
   $('#beat-spotlight').fadeIn(2000);
   $('#releases-spotlight').fadeIn(2500);
