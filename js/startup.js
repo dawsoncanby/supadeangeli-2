d185d408e09cdac97df780114b95589a7c4bf3b6
dawsoncanby <dawsoncanby@gmail.com>
2019-11-05 09:39:58 -0700

    added skeletal

--- js/startup.js
+++ js/startup.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-  // add images
+  // add images/gifs
   $('#release-img-container').prepend('<img src="img/talk_that_way.jpg" alt="new release" style="display: block;margin: auto;"></img>');
-  $('#release-img-container').click(function() {
-    
-  });
+  $('#release-gif-container').prepend('<img src="img/skeletal.gif" alt="new release" style="display: block;margin: auto;"></img>');
+
   // fade in content
   $('#page-header').fadeIn(1000);
   $('#beat-spotlight').fadeIn(2000);
