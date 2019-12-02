7a037b15234a9de5163262cb1bb1ced61e72fcc1
Dawson Canby <dawsoncanby@gmail.com>
2019-12-02 16:21:01 -0700

    winston

--- js/linkModal.js
+++ js/linkModal.js
@@ -9,6 +9,10 @@ var LinkModal = {
 var linkModals = {
   "talk that way": {
     "message": "Select a Streaming Service",
-    "title": "Talk That Way - Supa Squidds"
+    "title": "Talk That Way - Supa Squidds",
+    "links": {
+      "soundcloud": "https://soundcloud.com/supasquidds/talk-that-way",
+      "spotify": "https://open.spotify.com/track/3vUcwnXfPwH8svPTp4Ez5o?si=b63N8wq3QuiRXY7AZkL-wA",
+      "apple music": "https://music.apple.com/us/album/talk-that-way-single/1481901449?app=music&ign-mpt=uo%3D4"
   }
 };
