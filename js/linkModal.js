efb7a14898aa094a9adfafc23693ab45194b5631
Dawson Canby <dawsoncanby@gmail.com>
2019-12-02 16:15:13 -0700

    started refactoring modal

---
+++ js/linkModal.js
@@ -0,0 +1,14 @@
+var LinkModal = {
+  // create a modal and add it to the page
+  createModal: function(modalObj) {
+
+  }
+};
+
+// modalObjs for different songs/videos
+var linkModals = {
+  "talk that way": {
+    "message": "Select a Streaming Service",
+    "title": "Talk That Way - Supa Squidds"
+  }
+};
