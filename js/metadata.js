41e2cbf09dc0266822f2e622b804f9b2d02a507e
dawsoncanby <dawsoncanby@gmail.com>
2019-12-09 19:09:52 -0700

    added images to carousel

--- js/metadata.js
+++ js/metadata.js
@@ -16,16 +16,29 @@ var linkMetaObjs = {
     "id": "talk-that-way",
     "message": "Select a Streaming Service",
     "title": "Talk That Way - Supa Squidds",
+    "img": "img/talk_that_way.jpg",
     "links": {
       "soundcloud": "https://soundcloud.com/supasquidds/talk-that-way",
       "spotify": "https://open.spotify.com/track/3vUcwnXfPwH8svPTp4Ez5o?si=b63N8wq3QuiRXY7AZkL-wA",
       "apple music": "https://music.apple.com/us/album/talk-that-way-single/1481901449?app=music&ign-mpt=uo%3D4"
     }
   },
+  "music for dogs": {
+    "id": "music-for-dogs",
+    "message": "Select a Streaming Service",
+    "title": "Music for Dogs- Supa Squidds",
+    "img": "img/music_for_dogs.png",
+    "links": {
+      "soundcloud": "https://soundcloud.com/supasquidds/sets/music-for-dogs",
+      "spotify": "https://open.spotify.com/album/4qK3aV9zjK6srRqWodzxCh?si=b2j8Nk_9SiKjlNjM-BMv_Q",
+      "apple music": "https://music.apple.com/us/album/music-for-dogs/1480442383?app=music"
+    }
+  },
   "skeletal": {
     "id": "skeletal",
     "message": "Watch Now",
     "title": "Skeletal - Supa Squidds",
+    "img": "img/skeletal.gif",
     "links": {
       "youtube": "https://youtu.be/ROygtYxgTr4"
     }
--- js/releaseCarousel.js
+++ js/releaseCarousel.js
@@ -6,13 +6,15 @@ var ReleaseCarousel = {
     Object.keys(releaseObjs).forEach((e, i) => {
       items = items +
       `<div class="carousel-item ${i == 0 ? 'active' : ''}">
-        <div class="release-item h-100">yerr</div>
+        <div class="release-item ">
+          <img src="${releaseObjs[e].img}">
+        </div>
       </div>`;
     });
 
     var carousel = $(`
       <div id="release-carousel" class="carousel slide h-100" data-ride="carousel">
-        <div class="carousel-inner h-100">
+        <div class="carousel-inner">
           ${items}
         </div>
         <a class="carousel-control-prev" href="#release-carousel" role="button" data-slide="prev">
